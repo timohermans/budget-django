d4c9486497a3cd23b2be11afad8564080558ed1c
Timo Hermans <tmhermans@gmail.com>
2026-04-19 14:55:46 +0200

    chore: add one more test for month subtraction method

--- config/tests.py
+++ config/tests.py
@@ -8,10 +8,11 @@ class DatesTestCase(unittest.TestCase):
     def test_subtract_one_month__when_february__returns_january(self):
         test_cases = [
             (date(2025, 2, 1), date(2025, 1, 1)),
-            (date(2025, 1, 1), date(2024, 12, 1))
+            (date(2025, 1, 1), date(2024, 12, 1)),
+            (date(2025, 3, 31), date(2025, 2, 28)),
         ]
 
         for (input_date, expected_date) in test_cases:
-            with self.subTest(f"{input_date.year}-{input_date.month} -> {expected_date.year}-{expected_date.month}"):
+            with self.subTest(f"{input_date.year}-{input_date.month}-{input_date.day} -> {expected_date.year}-{expected_date.month}-{expected_date.day}"):
                 result = subtract_one_month(input_date)
                 self.assertEqual(expected_date, result)
